b51843610336c465c32dbc8754a20c729b4c397b
Ahmed Softic <asoftic1@etf.unsa.ba>
2025-04-04 01:07:24 +0200

    migration + connection setup

---
+++ database/migrate.js
@@ -0,0 +1,32 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { up } = require('migrate-mongo');
+require('dotenv').config();
+
+const isLocal = process.env.USE_LOCAL_DB === "true";
+const dbUri = isLocal ? process.env.DB_URI_LOCAL : process.env.DB_URI;
+
+const client = new MongoClient(dbUri, {
+  tlsAllowInvalidCertificates: true,
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function migrate() {
+    try {
+        console.log("Connecting to MongoDB...");
+        await client.connect();
+        const db = client.db(process.env.DB_NAME || "SecureRemoteControl");
+
+        console.log("Running migrations...");
+        await up(db, client); 
+
+        console.log("Migrations applied successfully!");
+    } catch (error) {
+        console.error("Error during migration:", error);
+    }
+}
+
+migrate();
