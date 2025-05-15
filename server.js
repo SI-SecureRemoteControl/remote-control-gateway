2564d976a633cbabe69443876b6b44eef0c39028
Ahmed Corhusic <acorhusic1@etf.unsa.ba>
2025-05-15 20:53:56 +0200

    Added required node modules

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
+const multer = require("multer");
 //const https = require("https");
 const http = require('http');
 const WebSocket = require("ws");
